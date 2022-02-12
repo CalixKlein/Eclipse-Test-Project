6b3563e423d07fe9f96c21a6abac3c06e37deea8
Sindongin <corean1225@naver.com>
2022-02-12 09:45:04 +0900

    2commit message

--- src/HelloGithub2.java
+++ src/HelloGithub2.java
@@ -1,10 +1,9 @@
 
-public class HelloGithub {
+public class HelloGithub2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HelloGitHub");
-		System.out.println("update");
 	}
 
 }
